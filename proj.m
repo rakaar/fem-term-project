b32ff56f2b30bead6bbaafb44c6f62aa170f7b55
rakaar <rka87338@gmail.com>
2022-10-26 19:47:34 +0530

    added displacement vec and force vec

--- proj.m
+++ proj.m
@@ -54,5 +54,25 @@ for r=1:size(element_coords,1)
     end
 end
 
+%% force vectors for all elements
+force_vecs = cell(size(element_coords,1), size(element_coords,2)); % each is 16x1 - [Fu1 Fv1 Fu2 Fv2 ... Fu8 Fv8]
+for r=1:size(element_coords,1)
+    for c=1:size(element_coords,2)
+        force_vecs{r,c} = zeros(16,1);
+    end
+end
 
+% except element at the right corner
+force_vecs{1,size(force_vecs,2)}(6,1) = F; % force applied - 3rd node, 2nd dof
+%% displacement F = KQ
+displacement_vecs = cell(size(element_coords,1), size(element_coords,2)); % each is 16x1 - [u1 v1 u2 v2 ... u8 v8]
+for r=1:size(element_coords,1)
+   
+    for c=1:size(element_coords,2)
+        stiffness_matrices{r,c} = make_stiffness_matrix(element_coords{r,c}, young_modulus, poisson_ratio);
+        displacement_vecs{r,c} = pinv(stiffness_matrices{r,c})*force_vecs{r,c};
+    end
+end
+
+%% calculate stress
 
