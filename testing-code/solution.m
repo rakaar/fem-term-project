969f24ee2cc3767b542fa76f1a35bc28b864207b
rakaar <rka87338@gmail.com>
2022-10-28 15:36:15 +0530

    stress matrices at nodes doc

--- testing-code/solution.m
+++
@@ -1,5 +0,0 @@
-Function[displacements]=solution(GDof,prescribedDof,...
- Stiffness, force)
-%freeDof= Active Dof
-freeDof=setdiff((1:GDof)',prescribedDof);
-displacements(freeDof)=inv(stiffness(freeDof,freeDof))*(force(freeDof));
